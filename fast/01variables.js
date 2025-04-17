d01fb7bfee3b3c5e68b9548320076f3a0ffe6d59
vihaaan <123803030+vihaaan@users.noreply.github.com>
2025-04-17 12:45:20 +0000

    done

---
+++ fast/01variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountemail = "vihaan.karada@email.com"
+var accountpassword = "12345"
+accountcity = 'indore'
+
+//accountId = 2// not allowed
+
+accountemail = "hc@email"
+accountpassword = "1434143"
+accountcity = "bengaluru"
+
+
+
+
+
+console.log(accountId);

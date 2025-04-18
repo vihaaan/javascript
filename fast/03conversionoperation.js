82f280791982003849cc4f839ed73e27c285b655
vihaaan <123803030+vihaaan@users.noreply.github.com>
2025-04-18 06:03:21 +0000

    done

---
+++ fast/03conversionoperation.js
@@ -0,0 +1,28 @@
+let score = 1
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//"33"=> 33
+//"33abc" => NaN
+//true => 1 , false => 0
+
+let IsLoggedIn = "vihaan"
+
+let booleanIsloggedIn = Boolean(IsLoggedIn);
+console.log(booleanIsloggedIn);
+
+// 1 = true; 0 = false
+//"vihaan" = true
+// "" = false
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+

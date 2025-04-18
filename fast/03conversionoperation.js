c259abddeb20a262cea0a35de73e08a117fd6e07
vihaaan <123803030+vihaaan@users.noreply.github.com>
2025-04-18 06:34:19 +0000

    done

--- fast/03conversionoperation.js
+++ fast/03conversionoperation.js
@@ -1,28 +1,45 @@
-let score = 1
+//let score = 1//
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);//
+//console.log(typeof (score));//
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//let valueInNumber = Number(score)//
+//console.log(typeof valueInNumber);//
+//console.log(valueInNumber);//
 
 //"33"=> 33
 //"33abc" => NaN
 //true => 1 , false => 0
 
-let IsLoggedIn = "vihaan"
+//let IsLoggedIn = "vihaan"//
 
-let booleanIsloggedIn = Boolean(IsLoggedIn);
-console.log(booleanIsloggedIn);
+//let booleanIsloggedIn = Boolean(IsLoggedIn);//
+//console.log(booleanIsloggedIn);//
 
 // 1 = true; 0 = false
 //"vihaan" = true
 // "" = false
-let someNumber = 33
+//let someNumber = 33//
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//let stringNumber = String(someNumber)//
+//console.log(stringNumber);//
+//console.log(typeof stringNumber);//
 
+// *******************OPERATIONS*******************//
 
+let value = 3
+ let negvalue = -value
+// console.log(negvalue);
+ 
+ //console.log(2 + 2);
+ //console.log(2 - 2);
+ //console.log(2 * 2);
+ //console.log(2 ** 2);
+//console.log(2/2);
+// console.log(2%2);
+ 
+let str1 = "hello"
+let str2  = "vihaan"
+let str3 = str1 + str2
+
+console.log(str3);

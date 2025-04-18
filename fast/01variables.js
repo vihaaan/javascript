c72ee9d7c060747590f8ae0a03a850540aa2f3ef
vihaaan <123803030+vihaaan@users.noreply.github.com>
2025-04-18 04:56:35 +0000

    dobne

--- fast/01variables.js
+++ fast/01variables.js
@@ -2,6 +2,7 @@ const accountId = 144553
 let accountemail = "vihaan.karada@email.com"
 var accountpassword = "12345"
 accountcity = 'indore'
+let accountstate; 
 
 //accountId = 2// not allowed
 
@@ -10,7 +11,12 @@ accountpassword = "1434143"
 accountcity = "bengaluru"
 
 
-
-
-
 console.log(accountId);
+
+/*
+prefer not to use var
+as of a scope problem
+*/
+
+
+console.table([accountId,accountemail,accountpassword,accountcity])
---
+++ fast/02datatypes.js
@@ -0,0 +1,46 @@
+"use strict"; // treat all js code as a newer version
+
+// alert( 3 + 3350) // not a broswer
+
+console.log(3 + 3)
+
+console.log("vihaan")
+
+
+let name = "vihaan"
+let age = 12
+let isloggedin = false
+
+
+// no => 2 to 53
+// bigint
+// string => ""
+// boolean =>true / false
+// null => standalone value
+// undefined => value not defineed
+// symbol =>. unique
+
+
+
+console.log(typeof null);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

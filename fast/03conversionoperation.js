92332685af447da5a1f7194b43067e7e221bcede
vihaaan <123803030+vihaaan@users.noreply.github.com>
2025-05-26 12:15:01 +0530

    Update 03conversionoperation.js
    done

--- fast/03conversionoperation.js
+++ fast/03conversionoperation.js
@@ -38,10 +38,13 @@ let value = 3
 //console.log(2/2);
 // console.log(2%2);
  
-let str1 = "hello"
-let str2  = "vihaan"
-let str3 = str1 + str2
+//let str1 = "hello"
+//let str2  = " vihaan"
+//let str3 = str1 + str2
 
-console.log(str3);
+//console.log(str3);
 
-console.log("1" + 2 + 2)
+//console.log("1" + 2 + 2)
+
+//console.log(1 + 2 + "2"
+console.log(true);

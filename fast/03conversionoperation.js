c5a47538259c7f60cbe742ce85178eb88215d528
vihaaan <123803030+vihaaan@users.noreply.github.com>
2025-05-26 17:02:49 +0530

    Update 03conversionoperation.js

--- fast/03conversionoperation.js
+++ fast/03conversionoperation.js
@@ -29,25 +29,22 @@
 
 let value = 3
  let negvalue = -value
-// console.log(negvalue);
+console.log(negvalue);
  
- //console.log(2 + 2);
- //console.log(2 - 2);
- //console.log(2 * 2);
- //console.log(2 ** 2);
-//console.log(2/2);
-// console.log(2%2);
+ console.log(2 + 2);
+ console.log(2 - 2);
+ console.log(2 * 2);
+ console.log(2 ** 2);
+console.log(2/2);
+ console.log(2%2);
  
-//let str1 = "hello"
-//let str2  = " vihaan"
-//let str3 = str1 + str2
+let str1 = "hello"
+let str2  = " vihaan"
+let str3 = str1 + str2
 
-//console.log(str3);
+console.log(str3);
 
-//console.log("1" + 2 + 2)
-
-//console.log(1 + 2 + "2"
-//console.log(true);
+console.log("1" + 2 + 2)
 
 let gameCounter = 100
 ++gameCounter

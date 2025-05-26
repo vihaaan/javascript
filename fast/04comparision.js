77a975ab779d7b892b363a42aa9db3be3a02d017
vihaaan <123803030+vihaaan@users.noreply.github.com>
2025-05-26 13:59:09 +0530

    done
    done

---
+++ fast/04comparision.js
@@ -0,0 +1,2 @@
+console.log(null == 0);
+console.log(null >= 0);
---
+++ fast/datatypessummary.js
@@ -0,0 +1,22 @@
+//primitives -> 7 types
+
+// bigint, symbol, null, undefined, number, boolean, string
+
+const bignumber = 478957934589365n
+
+const outsidetemp = null
+
+//reference (non primitive) all will come object in console.log typeof
+
+
+//array, functions, object
+
+const heros = ["captain underpants","sciencet" ]
+ {
+name:"vihaan"
+Age:"12"
+}
+//it can be any string number and more
+
+console.log(typeof outsidetemp);
+

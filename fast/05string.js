edc3b77d0029b843bbd2656ef92b0221b96a4a45
vihaaan <123803030+vihaaan@users.noreply.github.com>
2025-05-30 16:02:36 +0530

    done

---
+++ fast/05string.js
@@ -0,0 +1,5 @@
+const Name = "vihaan"
+const RepoCount = " 3"
+//console.log(Name + RepoCount + "value"); -. outdatedlog
+
+//console.log(`hello my name is ${Name} and my repocount is ${RepoCount}`); -> up to datev 
---
+++ fast/06numnmaths.js
@@ -0,0 +1,7 @@
+console.log(Math.floor(Math.random()*10));
+
+const num = 10
+const othernum = 20
+
+console.log(Math.random()* (num - othernum + 1) + num)
+
